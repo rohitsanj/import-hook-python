ab4fc507fed5c5de5d350b5ce92abda92f55c3bb
Rohit Sanjay <rohit.sanjay@noteable.io>
2022-05-19 22:43:11 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Import hooks in Python
+
+This repository contains an implementation of a custom importer (finder and loader) that imports code directly from an in-memory SQLite database.
+
+# How to use
+
+```python
+python -i import_hook.py # Opens up an interactive shell with the code in import_hook executed
+```
+
+# References
+- ["Hooking into the import system" - Fred Phillips at PyCon US 2022](https://us.pycon.org/2022/schedule/presentation/76/)
+- [The import system - Python Language Reference](https://docs.python.org/3/reference/import.html#the-import-system)
+
---
+++ import_hook.py
@@ -0,0 +1,75 @@
+import sqlite3
+import importlib.abc
+
+foo_code = """
+print("foo is being initialized!")
+
+def add(a, b):
+    return a + b
+
+"""
+
+bar_code = """
+print("bar is being initialized")
+
+def sub(a, b):
+    return a - b
+
+"""
+
+spam_code = """
+print("spam is being initialized")
+
+def fib(n: int):
+    if n in (0, 1):
+        return n
+
+    return fib(n-1) + fib(n-2)
+"""
+
+
+def create_repo_table():
+    con = sqlite3.connect(":memory:")
+    cursor = con.cursor()
+    cursor.execute("create table repository (fullname, source_code)")
+    cursor.executemany(
+        "insert into repository values (?, ?)",
+        [("foo", foo_code), ("bar", bar_code), ("spam", spam_code)],
+    )
+
+    con.commit()
+    return cursor
+
+
+class DBFinder(importlib.abc.MetaPathFinder):
+    def __init__(self, cursor: sqlite3.Cursor):
+        self.cursor = cursor
+
+    def find_spec(self, fullname: str, path, target):
+        # Query database to see if we have the `fullname` module
+        from importlib.machinery import ModuleSpec
+
+        self.cursor.execute("select 1 from repository where fullname = ?", (fullname,))
+        if self.cursor.fetchone():
+            return ModuleSpec(fullname, DBLoader(cursor=self.cursor))
+
+        # We don't have it in the database, go look elsewhere
+        return None
+
+
+class DBLoader(importlib.abc.Loader):
+    def __init__(self, cursor: sqlite3.Cursor) -> None:
+        self.cursor = cursor
+
+    def exec_module(self, module):
+        self.cursor.execute(
+            "select source_code from repository where fullname = ?", (module.__name__,)
+        )
+        source_code = self.cursor.fetchone()
+        exec(source_code[0], module.__dict__)
+
+
+import sys
+
+cursor = create_repo_table()
+sys.meta_path.insert(0, DBFinder(cursor=cursor))
